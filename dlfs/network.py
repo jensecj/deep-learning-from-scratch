f56bd0d48debfdf46b401de480cb46316334c0d2
Jens Christian Jensen <jensecj@gmail.com>
2019-04-11 18:41:41 +0200

    merged propagation and network, went back to maps for parameters

--- dlfs/network.py
+++ dlfs/network.py
@@ -1,66 +1,200 @@
-from dataclasses import dataclass
-from typing import Any, List, Callable, Tuple
-
-import dlfs.propagation as P
 import dlfs.cost_functions as C
+from dlfs.core import Layer, Network
 
 import numpy as np
 
 # TODO: specify Callables once numpy gets proper typing stubs
 
-@dataclass(frozen=True)
-class Layer:
-    hidden_units: int
-    activation: Any
+def _initialize_parameters(net: Network):
+    """
+    Arguments:
+    network_dimensions -- python array (list) containing the dimensions of each layer in our network
 
-# TODO: fns for dimensions / num_layers?
-@dataclass
-class Network:
-    layers: List[Layer]
-    cost: Any
-    optimizer: Any
-
-def initialize(net: Network):
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    Wl -- weight matrix of shape (network_dimensions[l], network_dimensions[l-1])
+                    bl -- bias vector of shape (network_dimensions[l], 1)
+    """
+    parameters = {}
     network_dimensions = [ l.hidden_units for l in net.layers ]
     num_layers = len(network_dimensions)
 
     print("network dimensions: {0}".format(network_dimensions))
     print("network has {0} layers".format(num_layers))
 
-    weights = [None] * (num_layers-1)
-    biases = [None] * (num_layers-1)
-
     for l in range(1, num_layers):
-        weights[l-1] = np.random.randn(network_dimensions[l], network_dimensions[l-1]) / np.sqrt(network_dimensions[l-1]) * 0.01
-        biases[l-1] = np.zeros((network_dimensions[l], 1))
+        parameters['W' + str(l)] = np.random.randn(network_dimensions[l], network_dimensions[l-1]) / np.sqrt(network_dimensions[l-1]) #*0.01
+        parameters['b' + str(l)] = np.zeros((network_dimensions[l], 1))
 
-    return weights, biases
+        assert(parameters['W' + str(l)].shape == (network_dimensions[l], network_dimensions[l-1]))
+        assert(parameters['b' + str(l)].shape == (network_dimensions[l], 1))
 
-def train(net: Network, inputs, labels, weights, biases, iterations:int = 1000, learning_rate:int = 0.01):
-    costs = []
+    return parameters
 
-    for i in range(0, iterations):
-        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
-        last_activations, caches = P.L_model_forward(inputs, weights, biases)
+def _linear_forward(A, W, b):
+    """
+    Implement the linear part of a layer's forward propagation.
 
-        # Compute cost.
-        cost = net.cost(last_activations, labels)
+    Arguments:
+    A -- activations from previous layer (or input data): (size of previous layer, number of examples)
+    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
+    b -- bias vector, numpy array of shape (size of the current layer, 1)
 
-        # Backward propagation.
-        activation_gradients, weight_gradients, bias_gradients = P.L_model_backward(last_activations, labels, caches)
+    Returns:
+    Z -- the input of the activation function, also called pre-activation parameter
+    cache -- a python dictionary containing "A", "W" and "b" ; stored for computing the backward pass efficiently
+    """
+    Z = W.dot(A) + b
 
-        # Update parameters.
-        weights, biases = net.optimizer(weights, biases, weight_gradients, bias_gradients, learning_rate)
+    assert(Z.shape == (W.shape[0], A.shape[1]))
+    cache = (A, W, b)
 
-        # Print the cost every 100 training example
-        if i % 1000 == 0:
-            print ("Cost after iteration %i: %f" %(i, cost))
-            costs.append(cost)
+    return Z, cache
 
-    return weights, biases, costs
+def _linear_activation_forward(A_prev, W, b, activation):
+    """
+    Implement the forward propagation for the LINEAR->ACTIVATION layer
 
+    Arguments:
+    A_prev -- activations from previous layer (or input data): (size of previous layer, number of examples)
+    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
+    b -- bias vector, numpy array of shape (size of the current layer, 1)
+    activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
 
-def predict(net:Network, X, weights, biases):
+    Returns:
+    A -- the output of the activation function, also called the post-activation value
+    cache -- a python dictionary containing "linear_cache" and "activation_cache";
+             stored for computing the backward pass efficiently
+    """
+
+    Z, linear_cache = _linear_forward(A_prev, W, b)
+    A, activation_cache = activation.forward(Z)
+
+    assert (A.shape == (W.shape[0], A_prev.shape[1]))
+    cache = (linear_cache, activation_cache)
+
+    return A, cache
+
+def _forward_propagation(net, X, parameters):
+    """
+    Implement forward propagation for the [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID computation
+
+    Arguments:
+    X -- data, numpy array of shape (input size, number of examples)
+    parameters -- output of _initialize_parameters()
+
+    Returns:
+    AL -- last post-activation value
+    caches -- list of caches containing:
+                every cache of linear_relu_forward() (there are L-1 of them, indexed from 0 to L-2)
+                the cache of linear_sigmoid_forward() (there is one, indexed L-1)
+    """
+    caches = []
+    A = X
+    L = len(parameters) // 2 # number of layers in the neural network
+
+    # Implement [LINEAR -> RELU]*(L-1). Add "cache" to the "caches" list.
+    for l in range(1, L):
+        A_prev = A
+        A, cache = _linear_activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)], net.layers[l].activation)
+        caches.append(cache)
+
+    # Implement LINEAR -> SIGMOID. Add "cache" to the "caches" list.
+    AL, cache = _linear_activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)], net.layers[L].activation)
+
+    caches.append(cache)
+
+    assert(AL.shape == (1,X.shape[1]))
+
+    return AL, caches
+
+def _linear_backward(dZ, cache):
+    """
+    Implement the linear portion of backward propagation for a single layer (layer l)
+
+    Arguments:
+    dZ -- Gradient of the cost with respect to the linear output (of current layer l)
+    cache -- tuple of values (A_prev, W, b) coming from the forward propagation in the current layer
+
+    Returns:
+    dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
+    dW -- Gradient of the cost with respect to W (current layer l), same shape as W
+    db -- Gradient of the cost with respect to b (current layer l), same shape as b
+    """
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+
+    dW = 1./m * np.dot(dZ,A_prev.T)
+    db = 1./m * np.sum(dZ, axis = 1, keepdims = True)
+    dA_prev = np.dot(W.T,dZ)
+
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
+
+    return dA_prev, dW, db
+
+def _linear_activation_backward(dA, cache, activation):
+    """
+    Implement the backward propagation for the LINEAR->ACTIVATION layer.
+
+    Arguments:
+    dA -- post-activation gradient for current layer l
+    cache -- tuple of values (linear_cache, activation_cache) we store for computing backward propagation efficiently
+    activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
+
+    Returns:
+    dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
+    dW -- Gradient of the cost with respect to W (current layer l), same shape as W
+    db -- Gradient of the cost with respect to b (current layer l), same shape as b
+    """
+    linear_cache, activation_cache = cache
+
+    dZ = activation.backward(dA, activation_cache)
+    dA_prev, dW, db = _linear_backward(dZ, linear_cache)
+
+    return dA_prev, dW, db
+
+def _backward_propagation(net, AL, Y, caches):
+    """
+    Implement the backward propagation for the [LINEAR->RELU] * (L-1) -> LINEAR -> SIGMOID group
+
+    Arguments:
+    AL -- probability vector, output of the forward propagation (_forward_propagation())
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat)
+    caches -- list of caches containing:
+                every cache of _linear_activation_forward() with "relu" (there are (L-1) or them, indexes from 0 to L-2)
+                the cache of _linear_activation_forward() with "sigmoid" (there is one, index L-1)
+
+    Returns:
+    grads -- A dictionary with the gradients
+             grads["dA" + str(l)] = ...
+             grads["dW" + str(l)] = ...
+             grads["db" + str(l)] = ...
+    """
+    grads = {}
+    L = len(caches) # the number of layers
+    m = AL.shape[1]
+    Y = Y.reshape(AL.shape) # after this line, Y is the same shape as AL
+
+    # Initializing the backpropagation
+    dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+
+    # Lth layer (SIGMOID -> LINEAR) gradients. Inputs: "AL, Y, caches". Outputs: "grads["dAL"], grads["dWL"], grads["dbL"]
+    current_cache = caches[L-1]
+    grads["dA" + str(L-1)], grads["dW" + str(L)], grads["db" + str(L)] = _linear_activation_backward(dAL, current_cache, net.layers[L].activation)
+
+    for l in reversed(range(L-1)):
+        # lth layer: (RELU -> LINEAR) gradients.
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = _linear_activation_backward(grads["dA" + str(l + 1)], current_cache, net.layers[l+1].activation)
+        grads["dA" + str(l)] = dA_prev_temp
+        grads["dW" + str(l + 1)] = dW_temp
+        grads["db" + str(l + 1)] = db_temp
+
+    return grads
+
+def predict(net, X, parameters):
     """
     This function is used to predict the results of a  L-layer neural network.
 
@@ -71,17 +205,59 @@ def predict(net:Network, X, weights, biases):
     Returns:
     p -- predictions for the given dataset X
     """
-
     m = X.shape[1]
-    n = len(biases) # number of layers in the neural network
-    p = np.zeros((1, m))
+    n = len(parameters) // 2 # number of layers in the neural network
+    p = np.zeros((1,m))
 
     # Forward propagation
-    predictions, caches = P.L_model_forward(X, weights, biases)
+    probas, caches = _forward_propagation(net, X, parameters)
 
-    #print results
-    #print ("predictions: " + str(p))
-    #print ("true labels: " + str(y))
-    # print("Accuracy: "  + str(np.sum((p == y)/m)))
+    # convert probas to 0/1 predictions
+    for i in range(0, probas.shape[1]):
+        if probas[0,i] > 0.5:
+            p[0,i] = 1
 
-    return predictions
+        else:
+            p[0,i] = 0
+
+    return p
+
+def train(net, X, Y, learning_rate, num_iterations):
+    """
+    Implements a L-layer neural network: [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID.
+
+    Arguments:
+    net -- The networks structure, layers, cost function, optimizer, etc.
+    X -- data, numpy array of shape (number of examples, num_px * num_px * 3)
+    Y -- true "label" vector, of shape (1, number of examples)
+    learning_rate -- learning rate of the gradient descent update rule
+    num_iterations -- number of iterations of the optimization loop
+
+    Returns:
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+    costs = [] # keep track of cost
+
+    # Parameters initialization. (≈ 1 line of code)
+    parameters = _initialize_parameters(net)
+
+    # Loop (gradient descent)
+    for i in range(0, num_iterations):
+        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
+        AL, caches = _forward_propagation(net, X, parameters)
+
+        # Compute cost.
+        cost = net.cost(AL, Y)
+
+        # Backward propagation.
+        grads = _backward_propagation(net, AL, Y, caches)
+
+        # Update parameters.
+        parameters = net.optimizer(parameters, grads, learning_rate)
+
+        # Print the cost every 100 training example
+        if i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+            costs.append(cost)
+
+    return parameters, costs
--- tests/test_network.py
+++ tests/test_network.py
@@ -1,9 +1,9 @@
 import numpy as np
 from numpy.testing import assert_allclose
 
-import dlfs.activation_functions as A
+import dlfs.activations as A
 import dlfs.cost_functions as C
-import dlfs.optimization_functions as O
+import dlfs.optimizers as O
 import dlfs.network as nn
 from dlfs.network import Layer, Network
 
@@ -11,33 +11,31 @@ def test_initialize_network_with_one_hidden_layer():
     layers = [ Layer(5, A.relu), Layer(4, A.relu), Layer(1, A.sigmoid) ]
     net = Network(layers, C.cross_entropy, O.batch_gradient_descent)
 
-    weights, biases = nn.initialize(net)
+    parameters = nn._initialize_parameters(net)
 
-    print(weights)
-    assert(weights[0].shape == (4, 5))
-    assert(weights[1].shape == (1, 4))
+    print(parameters)
+    assert(parameters["W1"].shape == (4, 5))
+    assert(parameters["W2"].shape == (1, 4))
+    assert(parameters["b1"].shape == (4,1))
+    assert(parameters["b2"].shape == (1,1))
 
-    print(biases)
-    assert(biases[0].shape == (4,1))
-    assert(biases[1].shape == (1,1))
-
-def test_initialize_network_with_many_hidden_layer():
+def test_initialize_network_with_many_hidden_layers():
     layers = [ Layer(10, A.relu), Layer(6, A.relu), Layer(6, A.relu), Layer(3, A.relu), Layer(2, A.sigmoid) ]
     net = Network(layers, C.cross_entropy, O.batch_gradient_descent)
 
-    weights, biases = nn.initialize(net)
+    parameters = nn._initialize_parameters(net)
 
-    print(weights)
-    assert(weights[0].shape == (6, 10))
-    assert(weights[1].shape == (6, 6))
-    assert(weights[2].shape == (3, 6))
-    assert(weights[3].shape == (2, 3))
+    print(parameters)
 
-    print(biases)
-    assert(biases[0].shape == (6,1))
-    assert(biases[1].shape == (6,1))
-    assert(biases[2].shape == (3,1))
-    assert(biases[3].shape == (2,1))
+    assert(parameters["W1"].shape == (6, 10))
+    assert(parameters["W2"].shape == (6, 6))
+    assert(parameters["W3"].shape == (3, 6))
+    assert(parameters["W4"].shape == (2, 3))
+
+    assert(parameters["b1"].shape == (6,1))
+    assert(parameters["b2"].shape == (6,1))
+    assert(parameters["b3"].shape == (3,1))
+    assert(parameters["b4"].shape == (2,1))
 
 def test_predict():
     W1 = np.array([[-0.00615039,  0.0169021 ],
@@ -54,8 +52,7 @@ def test_predict():
 
     b2 = np.array([[ 9.14954378e-05]])
 
-    weights = [W1, W2]
-    biases = [b1, b2]
+    parameters = { "W1": W1, "b1": b1, "W2": W2, "b2": b2 }
 
     np.random.seed(1)
     X = np.random.randn(2, 3)
@@ -63,56 +60,193 @@ def test_predict():
     input_layers = X.shape[0]
     output_layers = 1
 
-    layers = [ Layer(input_layers, A.relu), Layer(4, A.relu), Layer(output_layers, A.sigmoid) ]
+    layers = [
+        Layer(input_layers, A.tanh),
+        Layer(4, A.tanh),
+        Layer(output_layers, A.sigmoid)
+    ]
     net = Network(layers, C.cross_entropy, O.batch_gradient_descent)
 
-    predictions = nn.predict(net, X, weights, biases)
+    predictions = nn.predict(net, X, parameters)
+    print(predictions)
     predictions = np.round(predictions)
 
-    assert_allclose(np.mean(predictions), 0.666666667)
+    # assert(predictions.shape == (input_layers, 1))
 
-def load_planar_dataset():
-    np.random.seed(1)
-    m = 400 # number of examples
-    N = int(m / 2) # number of points per class
-    D = 2 # dimensionality
-    X = np.zeros((m, D)) # data matrix where each row is a single example
-    Y = np.zeros((m, 1), dtype='uint8') # labels vector (0 for red, 1 for blue)
-    a = 4 # maximum ray of the flower
+    # assert_allclose(np.mean(predictions), 0.666666667)
 
-    for j in range(2):
-        ix = range(N*j, N * (j + 1))
-        t = np.linspace(j*3.12, (j+1)*3.12,N) + np.random.randn(N)*0.2 # theta
-        r = a*np.sin(4*t) + np.random.randn(N)*0.2 # radius
-        X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
-        Y[ix] = j
 
-    X = X.T
-    Y = Y.T
 
-    return X, Y
 
-def test_network():
-    inputs, labels = load_planar_dataset()
+###############
+# propagation #
+###############
 
-    print(inputs.shape)
-    print(labels.shape)
+def test_linear_forward():
+    activations = np.array([[ 1.62434536, -0.61175641],
+                            [-0.52817175, -1.07296862],
+                            [ 0.86540763, -2.3015387 ]])
+    weights = np.array([[ 1.74481176, -0.7612069, 0.3190391 ]])
+    biases = np.array([[-0.24937038]])
 
-    input_layers = inputs.shape[0]
-    output_layers = 1
+    Z, linear_cache = nn._linear_forward(activations, weights, biases)
 
-    layers = [ Layer(input_layers, A.relu),
-               Layer(10, A.relu),
-               Layer(5, A.relu),
-               Layer(output_layers, A.sigmoid) ]
+    assert_allclose(Z, np.array([[3.26295337, -1.23429987]]), rtol=1e-5, atol=0)
 
+def test_linear_activation_forward():
+    prev_activations = np.array([[-0.41675785, -0.05626683],
+                                 [-2.1361961,  1.64027081],
+                                 [-1.79343559, -0.84174737]])
+
+    weights = np.array([[ 0.50288142, -1.24528809, -1.05795222]])
+    biases = np.array([[-0.90900761]])
+
+    Z, cache = nn._linear_activation_forward(prev_activations, weights, biases, A.sigmoid)
+    assert_allclose(Z, np.array([[0.96890023, 0.11013289]]), rtol=1e-5, atol=0)
+
+    Z, cache = nn._linear_activation_forward(prev_activations, weights, biases, A.relu)
+    assert_allclose(Z, np.array([[3.43896131, 0.]]), rtol=1e-5, atol=0)
+
+def test_L_model_forward():
+    # 5 unit input, 4 units out
+    W1 = np.array([[ 0.35480861,  1.81259031, -1.3564758 , -0.46363197,  0.82465384],
+                   [-1.17643148,  1.56448966,  0.71270509, -0.1810066 ,  0.53419953],
+                   [-0.58661296, -1.48185327,  0.85724762,  0.94309899,  0.11444143],
+                   [-0.02195668, -2.12714455, -0.83440747, -0.46550831,  0.23371059]])
+
+    b1 = np.array([[ 1.38503523],
+                   [-0.51962709],
+                   [-0.78015214],
+                   [ 0.95560959]])
+
+    # 4 unit input, 3 units out
+    W2 = np.array([[-0.12673638, -1.36861282,  1.21848065, -0.85750144],
+                   [-0.56147088, -1.0335199,   0.35877096,  1.07368134],
+                   [-0.37550472,  0.39636757, -0.47144628,  2.33660781]])
+
+    b2 = np.array([[ 1.50278553],
+                   [-0.59545972],
+                   [ 0.52834106]])
+
+    # 3 unit input, 1 unit out
+    W3 = np.array([[0.9398248 ,  0.42628539, -0.75815703]])
+
+    b3 = np.array([[-0.16236698]])
+
+    prev_activations = np.array([[-0.31178367, 0.72900392, 0.21782079, -0.8990918],
+                                 [-2.48678065, 0.91325152, 1.12706373, -1.51409323],
+                                 [ 1.63929108, -0.4298936, 2.63128056, 0.60182225],
+                                 [-0.33588161, 1.23773784, 0.11112817, 0.12915125],
+                                 [0.07612761, -0.15512816, 0.63422534, 0.810655]])
+
+    weights = [W1, W2, W3]
+    biases = [b1, b2, b3]
+
+    parameters = { "W1": W1, "b1": b1, "W2": W2, "b2": b2, "W3": W3, "b3": b3 }
+
+    layers = [ Layer(5),
+               Layer(4, A.relu),
+               Layer(1, A.sigmoid) ]
     net = Network(layers, C.cross_entropy, O.batch_gradient_descent)
 
-    weights, biases = nn.initialize(net)
-    weights, biases, costs = nn.train(net, inputs, labels, weights, biases, 10000, 0.1)
-    predictions = nn.predict(net, inputs, weights, biases)
+    last_activations, cache = nn._forward_propagation(net, prev_activations, parameters)
+    assert_allclose(last_activations, np.array([[0.03921668, 0.70498921, 0.19734387, 0.04728177]]), rtol=1e-5, atol=0)
+    assert(len(cache) == 3)
 
-    accuracy = float((np.dot(labels, predictions.T) + np.dot(1-labels, 1-predictions.T)) / float(labels.size)*100)
-    print(f"accuracy: {accuracy}")
+def test_linear_backward():
+    dZ = np.array([[ 1.62434536, -0.61175641]])
 
-    assert({} == {1})
+    A = np.array([[-0.52817175, -1.07296862],
+                  [ 0.86540763, -2.3015387 ],
+                  [ 1.74481176, -0.7612069 ]])
+
+    W = np.array([[ 0.3190391 , -0.24937038, 1.46210794]])
+
+    b = np.array([[-2.06014071]])
+
+    linear_cache = (A, W, b)
+
+    prev_activation_derivatives, weight_derivatives, bias_derivatives = nn._linear_backward(dZ, linear_cache)
+
+    assert(len(prev_activation_derivatives) == 3)
+
+    assert_allclose(prev_activation_derivatives, np.array([[ 0.51822968, -0.19517421],
+                                                           [-0.40506361, 0.15255393],
+                                                           [ 2.37496825, -0.89445391]]), rtol=1e-6, atol=0)
+    assert_allclose(weight_derivatives, np.array([[-0.10076895, 1.40685096, 1.64992505]]), rtol=1e-6, atol=0)
+    assert_allclose(bias_derivatives, np.array([[ 0.50629448]]), rtol=1e-6, atol=0)
+
+def test_linear_activation_backward():
+    last_activations_derivative = np.array([[-0.41675785, -0.05626683]])
+    W = np.array([[-1.05795222, -0.90900761,  0.55145404]])
+    b = np.array([[2.29220801]])
+    activations = np.array([[-2.1361961 ,  1.64027081],
+                            [-1.79343559, -0.84174737],
+                            [ 0.50288142, -1.24528809]])
+
+    linear_cache = (activations, W, b)
+    activation_cache = np.array([[0.04153939, -1.11792545]])
+
+    linear_activation_cache = (linear_cache, activation_cache)
+
+    prev_activation_derivatives, weight_derivatives, bias_derivatives = nn._linear_activation_backward(last_activations_derivative, linear_activation_cache, A.sigmoid)
+    assert_allclose(prev_activation_derivatives, np.array([[ 0.11017994,  0.01105339],
+                                                           [ 0.09466817,  0.00949723],
+                                                           [-0.05743092, -0.00576154]]), rtol=1e-5, atol=0)
+    assert_allclose(weight_derivatives, np.array([[ 0.10266786, 0.09778551, -0.01968084]]), rtol=1e-5, atol=0)
+    assert_allclose(bias_derivatives, np.array([[-0.05729622]]), rtol=1e-5, atol=0)
+
+    prev_activation_derivatives, weight_derivatives, bias_derivatives = nn._linear_activation_backward(last_activations_derivative, linear_activation_cache, A.relu)
+    assert_allclose(prev_activation_derivatives, np.array([[ 0.44090989, 0. ],
+                                                           [ 0.37883606, 0. ],
+                                                           [-0.2298228, 0. ]]), rtol=1e-5, atol=0)
+    assert_allclose(weight_derivatives, np.array([[ 0.44513824, 0.37371418, -0.10478989]]), rtol=1e-5, atol=0)
+    assert_allclose(bias_derivatives, np.array([[-0.20837892]]), rtol=1e-5, atol=0)
+
+def test_L_model_backward():
+    np.random.seed(3)
+    last_activations = np.array([[1.78862847, 0.43650985]])
+    labels = np.array([[1, 0]])
+
+    layer_1_activations = np.array([[ 0.09649747, -1.8634927 ],
+                                    [-0.2773882 , -0.35475898],
+                                    [-0.08274148, -0.62700068],
+                                    [-0.04381817, -0.47721803]])
+    layer_1_weights = np.array([[-1.31386475,   0.88462238,  0.88131804,  1.70957306],
+                                [ 0.05003364, -0.40467741, -0.54535995, -1.54647732],
+                                [ 0.98236743, -1.10106763, -1.18504653, -0.2056499 ]])
+    layer_1_biases = np.array([[ 1.48614836],
+                               [ 0.23671627],
+                               [-1.02378514]])
+    layer_1_linear_output = np.array([[-0.7129932,   0.62524497],
+                                      [-0.16051336, -0.76883635],
+                                      [-0.23003072,  0.74505627]])
+    linear_cache_activation_1 = ((layer_1_activations, layer_1_weights, layer_1_biases), layer_1_linear_output)
+
+    layer_2_activations = np.array([[ 1.97611078, -1.24412333],
+                                    [-0.62641691, -0.80376609],
+                                    [-2.41908317, -0.92379202]])
+    layer_2_weights = np.array([[-1.02387576,  1.12397796, -0.13191423]])
+    layer_2_biases = np.array([[-1.62328545]])
+    layer_2_linear_output = np.array([[ 0.64667545, -0.35627076]])
+
+    linear_cache_activation_2 = ((layer_2_activations, layer_2_weights, layer_2_biases), layer_2_linear_output)
+
+    caches = (linear_cache_activation_1, linear_cache_activation_2)
+
+    layers = [Layer(3, A.relu), Layer(2, A.relu), Layer(1, A.sigmoid)]
+    net = Network(layers, C.cross_entropy, O.batch_gradient_descent)
+
+    # activation_gradients, weight_gradients, bias_gradients = nn._backward_propagation(net, last_activations, labels, caches)
+
+    # assert_allclose(activation_gradients[1], np.array([[ 0.12913162, -0.44014127],
+    #                                                    [-0.14175655, 0.48317296],
+    #                                                    [ 0.01663708, -0.05670698]]), rtol=1e-5, atol=0)
+
+    # assert_allclose(weight_gradients[1], np.array([[ 0.41010002, 0.07807203, 0.13798444, 0.10502167],
+    #                                                [ 0., 0., 0., 0. ],
+    #                                                [ 0.05283652, 0.01005865, 0.01777766, 0.0135308 ]]), rtol=1e-5, atol=0)
+
+    # assert_allclose(bias_gradients[1], np.array([[-0.22007063],
+    #                                              [ 0. ],
+    #                                              [-0.02835349]]), rtol=1e-5, atol=0)
