950ec857e443dd9f510e24be358974e24b79acb8
Jens Christian Jensen <jensecj@gmail.com>
2019-04-11 18:50:59 +0200

    fixed some tests in test_optimizers

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -19,8 +19,7 @@ def test_batch_gradient_descent():
     W2 = np.array([[-0.5961597, -0.0191305,  1.17500122]])
     b2 = np.array([[-0.74787095]])
 
-    weights = [W1, W2]
-    biases = [b1, b2]
+    parameters = { "W1": W1, "b1": b1, "W2": W2, "b2": b2 }
 
     dW1 = np.array([[ 1.78862847,  0.43650985,  0.09649747, -1.8634927 ],
                     [-0.2773882 , -0.35475898, -0.08274148, -0.62700068],
@@ -33,19 +32,18 @@ def test_batch_gradient_descent():
     dW2 = np.array([[-0.40467741, -0.54535995, -1.54647732]])
     db2 = np.array([[0.98236743]])
 
-    # FIXME: use lists for gradients
-    weight_gradients = [0,dW1, dW2]
-    bias_gradients = [0,db1, db2]
 
-    weights, biases = O.batch_gradient_descent(weights, biases, weight_gradients, bias_gradients, 0.1)
+    grads = { "dW1": dW1, "db1": db1, "dW2": dW2, "db2": db2 }
 
-    assert_allclose(weights[0], np.array([[-0.59562069, -0.09991781, -2.14584584, 1.82662008],
-                                          [-1.76569676, -0.80627147, 0.51115557, -1.18258802],
-                                          [-1.0535704, -0.86128581, 0.68284052, 2.20374577]]), rtol=1e-5, atol=0)
+    parameters = O.batch_gradient_descent(parameters, grads, 0.1)
 
-    assert_allclose(biases[0], np.array([[-0.04659241],
-                                         [-1.28888275],
-                                         [ 0.53405496]]), rtol=1e-5, atol=0)
+    assert_allclose(parameters["W1"], np.array([[-0.59562069, -0.09991781, -2.14584584, 1.82662008],
+                                                [-1.76569676, -0.80627147, 0.51115557, -1.18258802],
+                                                [-1.0535704, -0.86128581, 0.68284052, 2.20374577]]), rtol=1e-5, atol=0)
 
-    assert_allclose(weights[1], np.array([[-0.55569196, 0.0354055, 1.32964895]]), rtol=1e-5, atol=0)
-    assert_allclose(biases[1], np.array([[-0.84610769]]), rtol=1e-5, atol=0)
+    assert_allclose(parameters["b1"], np.array([[-0.04659241],
+                                                [-1.28888275],
+                                                [ 0.53405496]]), rtol=1e-5, atol=0)
+
+    assert_allclose(parameters["W2"], np.array([[-0.55569196, 0.0354055, 1.32964895]]), rtol=1e-5, atol=0)
+    assert_allclose(parameters["b2"], np.array([[-0.84610769]]), rtol=1e-5, atol=0)
