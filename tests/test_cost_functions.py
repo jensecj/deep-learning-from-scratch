f573aa3ce58be4cf4575b596df124070c68138fe
Jens Christian Jensen <jensecj@gmail.com>
2019-04-12 12:14:37 +0200

    updated tests for cost functions

--- tests/test_cost_functions.py
+++ tests/test_cost_functions.py
@@ -1,10 +1,51 @@
+import pytest
 import numpy as np
 from numpy.testing import assert_allclose
 
-from dlfs.cost_functions import *
+import dlfs.cost_functions as C
 
-def test_cross_entropy():
-    labels = np.asarray([[1, 1, 1]])
+#################
+# cross entropy #
+#################
+
+def test_cross_entropy_with_zero_predictions_zero_labels():
+    predictions = np.array([[0, 0]])
+    labels = np.array([[0, 0]])
+
+    cost = C.cross_entropy(predictions, labels)
+    assert_allclose(cost, 1e-12)
+
+def test_cross_entropy_with_column_vector():
+    predictions = np.array([[.8],
+                            [.9],
+                            [0.4]])
+    labels = np.array([[1],
+                       [1],
+                       [1]])
+
+    with pytest.raises(AssertionError):
+        cost = C.cross_entropy(predictions, labels)
+
+def test_cross_entropy_with_row_vector():
     predictions = np.array([[.8, .9, 0.4]])
+    labels = np.array([[1, 1, 1]])
 
-    assert_allclose(cross_entropy(predictions, labels), [0.41493159961539694])
+    cost = C.cross_entropy(predictions, labels)
+
+    assert_allclose(cost, 0.41493159961539694)
+
+def test_cross_entropy_with_predictions_greater_than_one():
+    predictions = np.array([[.8, 1.2, 1.4]])
+    labels = np.array([[1, 1, 1]])
+
+    cost = C.cross_entropy(predictions, labels)
+
+    assert_allclose(cost, 0.07438118377)
+
+def test_cross_entropy_with_predictions_less_than_0():
+    predictions = np.array([[.8, -0.532, 1.4]])
+    labels = np.array([[1, 1, 1]])
+
+    cost = C.cross_entropy(predictions, labels)
+
+    assert_allclose(cost, 0.407714666667)
